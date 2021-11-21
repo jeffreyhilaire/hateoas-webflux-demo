7680e0889cc4092fa670170d25ee18916d5cf39f
Ludovic Hilaire <jeffreyhilaire@gmail.com>
2021-11-21 15:57:42 -0500

    If applied, adds spring-hateoas to the sample application

--- src/main/java/com/example/demo/ApiHandler.java
+++ src/main/java/com/example/demo/ApiHandler.java
@@ -1,35 +1,25 @@
 package com.example.demo;
 
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 @Component
 public class ApiHandler {
 
     public Mono<ServerResponse> index(ServerRequest request) {
-        final Map<String,Object> model = new HashMap<>();
-        final List<Object> links = new ArrayList<>();
-        links.add(new HashMap<>(){{
-            put("href", URI.create("http://localhost:8080/accounts"));
-            put("rel","accounts");
-        }});
-        links.add(new HashMap<>(){{
-            put("href", URI.create("http://localhost:8080/roles"));
-            put("rel","roles");
-        }});
-        model.put("links",links);
-        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(
-                Mono.just(model),
-                Map.class
-        );
+        final RepresentationModel<?> model = new RepresentationModel<>();
+        model.add(Link.of("http://localhost:8080/accounts","accounts"));
+        model.add(Link.of("http://localhost:8080/roles","roles"));
+        return ServerResponse.ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(
+                    Mono.just(model),
+                    RepresentationModel.class
+            );
     }
 }
